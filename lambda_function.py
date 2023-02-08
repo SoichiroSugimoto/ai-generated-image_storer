524a6b3a52eb7496ef32bccda64f79004e2b63eb
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-02-09 01:39:13 +0900

    save image

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,13 @@
 import io
 import os
-import datetime
+import time
 import boto3
 from PIL import Image
 from stability_sdk import client
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 
+s3 = boto3.resource('s3')
+BUCKET_NAME = "ai-generated-image"
 
 def lambda_handler(event, context):
   stability_api = client.StabilityInference(
@@ -14,13 +16,22 @@ def lambda_handler(event, context):
   )
 
   answers = stability_api.generate(
-    prompt="Beautiful mirror under the sea",
+    prompt="Beautiful mirror under the sea"
   )
 
   for resp in answers:
     for artifact in resp.artifacts:
       if artifact.finish_reason == generation.FILTER:
         print("NSFW")
+        return {
+        'statusCode': 204
+        }
       if artifact.type == generation.ARTIFACT_IMAGE:
         img = Image.open(io.BytesIO(artifact.binary))
-        img.save('output.png')
+        bucket = s3.Bucket(BUCKET_NAME)
+        object_key_name = str(time.time()) + '.png'
+        obj = bucket.Object(object_key_name)
+        r = obj.put(img)
+        return {
+        'statusCode': 200
+        }
