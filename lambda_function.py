95d864859d1267720e95f37a3758ca624d3e4e54
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-02-09 01:13:46 +0900

    delete comment out text

--- lambda_function.py
+++ lambda_function.py
@@ -11,22 +11,19 @@ import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 
 
 def lambda_handler(event, context):
-  # APIインタフェースの準備
   stability_api = client.StabilityInference(
-      key=os.environ['STABILITY_KEY'],
-      verbose=True,
+    key=os.environ['STABILITY_KEY'],
+    verbose=True,
   )
 
-  # テキストからの画像生成
   answers = stability_api.generate(
-      prompt="Beautiful mirror under the sea",
+    prompt="Beautiful mirror under the sea",
   )
 
-  # 結果の出力
   for resp in answers:
-      for artifact in resp.artifacts:
-          if artifact.finish_reason == generation.FILTER:
-              print("NSFW")
-          if artifact.type == generation.ARTIFACT_IMAGE:
-              img = Image.open(io.BytesIO(artifact.binary))
-              img.save('output.png')
+    for artifact in resp.artifacts:
+      if artifact.finish_reason == generation.FILTER:
+        print("NSFW")
+      if artifact.type == generation.ARTIFACT_IMAGE:
+        img = Image.open(io.BytesIO(artifact.binary))
+        img.save('output.png')
