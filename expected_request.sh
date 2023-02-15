8c386cc421004760e30c06a64fd164d6485d88ed
SoichiroSugimoto <fcsoichi1998@gmail.com>
2023-02-15 23:54:49 +0900

    change expected http request

--- expected_request.sh
+++ expected_request.sh
@@ -1,4 +1,4 @@
-$url="https://89na8c39a1.execute-api.ap-northeast-1.amazonaws.com/srtability-sdk-spi"
-$prompt="xxxxxxxxxxxxxxxx"
-$TOKEN="***********"
-curl -g -X POST -H "Authorization: bearer $TOKEN" "$url?prompt=$prompt"
+url="https://89na8c39a1.execute-api.ap-northeast-1.amazonaws.com/srtability-sdk-spi"
+prompt='Many cats arerunning in the car'
+TOKEN="***********"
+curl -g -X POST -H "Authorization: bearer $TOKEN" -d "prompt=$prompt" "$url"
